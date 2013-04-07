03af7b0d80b26cd831f237b7e18bd988ab572f71
Martijn Natte <mnatte@gmail.com>
2013-04-07 23:07:54 +0200

    used velocity formula from stackoverflow

--- Mnd.Helpers/Helpers.cs
+++ Mnd.Helpers/Helpers.cs
@@ -4,6 +4,12 @@ using System.Linq;
 
 namespace Mnd.Helpers
 {
+    public class XYPoint
+    {
+        public int X;
+        public double Y;
+    }
+
     public static class DateTimeHelpers
     {
         /// <summary>
@@ -25,6 +31,27 @@ namespace Mnd.Helpers
         public static string ToDutchString(this DateTime date)
         {
             return string.Format("{0}/{1}/{2}", date.Day, date.Month, date.Year);
+        }
+    }
+
+    public static class MathHelpers
+    {
+        public static List<XYPoint> GenerateLinearBestFit(List<XYPoint> points, out double a, out double b)
+        {
+            int numPoints = points.Count;
+            double meanX = points.Average(point => point.X);
+            double meanY = points.Average(point => point.Y);
+
+            double sumXSquared = points.Sum(point => point.X * point.X);
+            double sumXY = points.Sum(point => point.X * point.Y);
+
+            a = (sumXY / numPoints - meanX * meanY) / (sumXSquared / numPoints - meanX * meanX);
+            b = (a * meanX - meanY);
+
+            double a1 = a;
+            double b1 = b;
+
+            return points.Select(point => new XYPoint() { X = point.X, Y = a1 * point.X - b1 }).ToList();
         }
     }
 }
--- Mnd.Planner.UseCases/Roles/RMilestoneStatus.cs
+++ Mnd.Planner.UseCases/Roles/RMilestoneStatus.cs
@@ -7,6 +7,8 @@ using Mnd.Planner.Domain;
 using System.Data.SqlClient;
 using Mnd.Planner.Domain.Persistence;
 using Mnd.Planner.Domain.Repositories;
+using Mnd.Helpers;
+using System.Diagnostics;
 
 namespace Mnd.Planner.UseCases.Roles
 {
@@ -44,6 +46,49 @@ namespace Mnd.Planner.UseCases.Roles
             var intercept = -((slope * avgDays) - avgHours);
 
             // formula best fitted line: amtHours = slope * amtDays + intercept
+        }
+
+        public static List<XYPoint> GetVelocityLine(this RMilestoneStatus ms, DateTime startDate)
+        {
+            var lst = new List<XYPoint>();
+            var rep = new ReleaseRepository();
+            var result = rep.GetArtefactsProgress(ms.Release.Id);
+
+            // determine amount days till milestone
+            var period = new Period { StartDate = startDate, EndDate = ms.Date };
+            var workingDays = period.WorkingDays;
+
+            // get progress data for milestone
+            var count = 0;
+
+            var points = result.Where(x => ms.Id == x.MilestoneId).Select(aProgress => new XYPoint { Y = aProgress.HoursRemaining, X = count++ }).ToList();
+            double a, b;
+
+            List<XYPoint> bestFit = MathHelpers.GenerateLinearBestFit(points, out a, out b);
+            Debug.WriteLine("y = {0:#.####}x {1:+#.####;-#.####}", a, -b);
+
+            for (int index = 0; index < points.Count; index++)
+            {
+                Debug.WriteLine("X = {0}, Y = {1}, Fit = {2:#.###}", points[index].X, points[index].Y, bestFit[index].Y);
+                lst.Add(new XYPoint { X = points[index].X, Y = bestFit[index].Y });
+            }
+            return lst;
+        }
+
+        public static List<XYPoint> GetBurndownLine(this RMilestoneStatus ms, DateTime startDate)
+        {
+            var rep = new ReleaseRepository();
+            var result = rep.GetArtefactsProgress(ms.Release.Id);
+
+            // determine amount days till milestone
+            var period = new Period { StartDate = startDate, EndDate = ms.Date };
+            var workingDays = period.WorkingDays;
+
+            // get progress data for milestone
+            var count = 0;
+
+            var points = result.Where(x => ms.Id == x.MilestoneId).Select(aProgress => new XYPoint { Y = aProgress.HoursRemaining, X = count++ }).ToList();
+            return points;
         }
     }
 }
